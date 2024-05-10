e983aae42120f8a5ff2c5a02195884fb7292e89e
Malak Saad <118460849+Saddeekhan@users.noreply.github.com>
2024-05-10 22:57:26 +0500

    Add files via upload

---
+++ 1. Packages.R
@@ -0,0 +1,33 @@
+
+install.packages("reshape2")
+install.packages("tidyverse")
+install.packages("phyloseq")
+install.packages("microbiome")
+install.packages("vegan")
+install.packages("readxl")
+install.packages("DESeq2")
+install.packages("picante")
+install.packages("metagenomeSeq")
+install.packages("HMP")
+install.packages("dendextend")
+install.packages("rms")
+install.packages("breakaway")
+install.packages("ALDEx2")
+install.packages("ggrepel")
+
+library(reshape2)  # Reshape data frames
+library(tidyverse)  # Collection of packages for data manipulation and visualization
+library(phyloseq)  # Microbiome data analysis
+library(microbiome)  # Microbiome data analysis
+library(vegan)  # Ecological community analysis
+library(readxl)  # Read Excel files
+library(DESeq2)  # Differential gene expression analysis
+library(picante)  # Phylogenetic integration and community analysis
+library(metagenomeSeq)  # Differential abundance analysis
+library(HMP)  # Hypothesis testing and sample size calculations
+library(dendextend)  # Hierarchical clustering and tree visualization
+library(rms)  # Regression modeling and prediction
+library(breakaway)  # Species richness estimation
+library(ALDEx2)  # Abundance inference from counts
+library(ggrepel)  # Enhanced ggplot2 graphics
+
