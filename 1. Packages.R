844007b2dce94a63d468c1b02aaa0a0aad5a1af1
Malak Saad <118460849+Saddeekhan@users.noreply.github.com>
2024-05-10 23:26:01 +0500

    1. Packages.R

--- 1. Packages.R
+++ 1. Packages.R
@@ -15,19 +15,5 @@ install.packages("breakaway")
 install.packages("ALDEx2")
 install.packages("ggrepel")
 
-library(reshape2)  # Reshape data frames
-library(tidyverse)  # Collection of packages for data manipulation and visualization
-library(phyloseq)  # Microbiome data analysis
-library(microbiome)  # Microbiome data analysis
-library(vegan)  # Ecological community analysis
-library(readxl)  # Read Excel files
-library(DESeq2)  # Differential gene expression analysis
-library(picante)  # Phylogenetic integration and community analysis
-library(metagenomeSeq)  # Differential abundance analysis
-library(HMP)  # Hypothesis testing and sample size calculations
-library(dendextend)  # Hierarchical clustering and tree visualization
-library(rms)  # Regression modeling and prediction
-library(breakaway)  # Species richness estimation
-library(ALDEx2)  # Abundance inference from counts
-library(ggrepel)  # Enhanced ggplot2 graphics
+
 
